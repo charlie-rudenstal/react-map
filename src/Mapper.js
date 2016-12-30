d94e3a4e8f9a2d67fb22c35f8956882182b9070f
Charlie Rudenstål <charlie4@gmail.com>
2016-12-30 20:35:15 +0100

    Add support for retrieving children

--- src/Mapper.js
+++ src/Mapper.js
@@ -1,17 +1,29 @@
 import fs from 'mz/fs';
 
-function getComponentList(path) {
-  return fs.readdir(path).then(listing => {
+function getComponents(dirPath) {
+  return fs.readdir(dirPath).then(listing => {
     return listing.map(filename => {
       const name = filename.slice(0, filename.lastIndexOf('.'));
       return {
         name,
-        path: `${path}/${filename}`
+        path: `${dirPath}/${filename}`
       };
     })
   });
 }
 
+function getChildren(filePath) {
+  return fs.readFile(filePath, 'utf-8').then(content => {
+    const tags = content.match(/<([^/][^>]+)>/g);
+    const tagNames = tags.map(tag => tag.slice(1, tag.indexOf(' ')));
+    const uniqueTagNames = [...new Set(tagNames)];
+    return uniqueTagNames.map(name => {
+      return { name };
+    });
+  });
+}
+
 export default {
-  getComponentList
+  getComponents,
+  getChildren
 }
--- test/test.js
+++ test/test.js
@@ -4,12 +4,20 @@ import mapper from '../src/Mapper';
 describe('react map', () => {
 
   it('should list all components', () => {
-    return mapper.getComponentList('./fixtures').then(components => {
+    return mapper.getComponents('./fixtures').then(components => {
       expect(components.length).toBe(3);
       expect(components).toInclude({name: 'Button', path: './fixtures/Button.js'});
       expect(components).toInclude({name: 'Tabs', path: './fixtures/Tabs.js'});
       expect(components).toInclude({name: 'Tab', path: './fixtures/Tab.js'});
     })
+  });
+
+  it('should list all child components', () => {
+    return mapper.getChildren('./fixtures/Tabs.js').then(children => {
+      expect(children.length).toBe(2);
+      expect(children).toInclude({name: 'div'});
+      expect(children).toInclude({name: 'Tab'});
+    });
   });
 
 });
