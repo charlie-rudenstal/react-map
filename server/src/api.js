46521e465030dc9d5fd7ebd39a198e9eee2eb1d7
Charlie Rudenstål <charlie4@gmail.com>
2017-01-02 22:00:56 +0100

    Add Component List API endpoint for Mapper

--- server/src/api.js
+++ server/src/api.js
@@ -1,9 +1,21 @@
 import mapper from './Mapper';
 import express from 'express';
 const api = express();
+const componentsPath = "server/test/fixtures";
+
+api.get('/components', (req,res) => {
+  mapper.getComponents(componentsPath)
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({error});
+  })
+  .then(components => {
+    res.json({ components })
+  });
+})
 
 api.get('/component/:path(*)', (req, res) => {
-  const path = req.params.path;
+  const path = `${componentsPath}/${req.params.path}`;
   const name = path.slice(path.lastIndexOf('/') + 1, path.lastIndexOf('.'));
   Promise.all([
     mapper.getDependencies(path),
--- server/test/api.js
+++ server/test/api.js
@@ -3,9 +3,22 @@ import request from 'supertest';
 import api from '../src/api';
 
 describe('api', () => {
+  it('should return a list of all components', done => {
+    request(api)
+      .get('/components')
+      .expect('Content-Type', /json/)
+      .expect(200,
+      {
+        components: [
+          { name: "Button", path: "server/test/fixtures/Button.js" },
+          { name: "Tab", path: "server/test/fixtures/Tab.js" },
+          { name: "Tabs", path: "server/test/fixtures/Tabs.js" }
+        ]
+      }, done);
+  })
   it('should return data about a component', done => {
     request(api)
-      .get('/component/server/test/fixtures/Tabs.js')
+      .get('/component/Tabs.js')
       .expect('Content-Type', /json/)
       .expect(200,
       {
@@ -19,6 +32,6 @@ describe('api', () => {
         classNames: [
           { name: "tabs" }, { name: "header" }, { name: "header--large" }
         ]
-      }, done)
+      }, done);
   });
 });
